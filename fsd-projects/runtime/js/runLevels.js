e2283e9a98400960a349c6aa76599c1d4f900951
leozhu12 <leoz091205@gmail.com>
2025-12-02 18:57:47 +0000

    enemy/colision

--- fsd-projects/runtime/js/runLevels.js
+++ fsd-projects/runtime/js/runLevels.js
@@ -18,8 +18,46 @@ var runLevels = function (window) {
 
     // TODOs 5 through 11 go here
     // BEGIN EDITING YOUR CODE HERE
+    function createObstacle(x, y, damage){
+    var hitZoneSize = 25; //sets the collide size
+    var damageFromObstacle = damage; //damage taken when collision happens
+    var obstacleHitZone = game.createObstacle(hitZoneSize, damageFromObstacle); //creates the hitzone and creates the collsion to take the damnge
+    obstacleHitZone.x = x; // sets the obsticle X position
+    obstacleHitZone.y = y; // sets the obsticle y position
+    game.addGameItem(obstacleHitZone); //adds the color to the playfield 
+    var obstacleImage = draw.bitmap("img/sawblade.png"); //draws the bitmap and stores it as an obsticle image
+    obstacleHitZone.addChild(obstacleImage); //takes the picture and add it has a child to the hitzone
+    obstacleImage.x = -25; //offsets the picture fron the hitzone
+    obstacleImage.y = -25; // offsets the obsticle image vertically
 
-    
+     obstacleHitZone.rotationalVelocity = 1000;
+    }
+
+    createObstacle(400, groundY - 110, 10);
+    createObstacle(600, groundY - 110, 50);
+    createObstacle(800, groundY - 110, 100);
+
+    var enemy = game.createGameItem("enemy", 25);
+    var redSquare = draw.rect(50, 50, "red");
+    redSquare.x = -25;
+    redSquare.y = -25;
+    enemy.addChild(redSquare);
+    enemy.x = 400;
+    enemy.y = groundY - 50;
+    game.addGameItem(enemy);
+
+    enemy.velocityX -= 3;
+
+    enemy.onPlayerCollision = function(){
+      game.changeIntegrity(-10);
+    }
+
+    enemy.onPlayerCollision = function(){
+      game.increaseScore(100);
+      enemy.fadeOut();
+      //enemy.shrink();
+      //enemy.flyTo(0,0);
+    }
 
     function startLevel() {
       // TODO 13 goes below here
--- fsd-projects/runtime/js/view/background.js
+++ fsd-projects/runtime/js/view/background.js
@@ -29,8 +29,8 @@ var background = function (window) {
         // ANIMATION VARIABLES HERE //////////////////////////////////////
         //////////////////////////////////////////////////////////////////
         // TODO (several):
-      
-      
+      var tree;
+      var buildings = [];
         // called at the start of game and whenever the page is resized
         // add objects for display in background. draws each image added to the background once
         function render() {
@@ -39,17 +39,41 @@ var background = function (window) {
             // TODO 1:
             // this currently fills the background with an obnoxious yellow;
             // you should modify both the height and color to suit your game
-            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'yellow');
-            background.addChild(backgroundFill);
+            var backgroundFill = draw.rect(canvasWidth,groundY,'blue'); // draws a rectangle and stores it inside background fill
+            background.addChild(backgroundFill); // adds the backgroundfill to the background
             
             // TODO 2: - Add a moon and starfield
+            for(var i = 0; i < 50; i++){
+                var circle = draw.circle(10, "white", "LightGray", 2); //creates a circle with a specificed radius, border color, fill color, alpha, and stores it in the variable circle. 
+                circle.x = canvasWidth * Math.random(); // sets a random x position within canvas width
+                circle.y = groundY * Math.random(); // sets a random y position within groundY
+                background.addChild(circle); // adds circle to the background container
+            }
+
+            var moon = draw.bitmap("img/moon.png"); //creates a bitmap object using the moon image and stores it in the variable moon
+            moon.x = canvas.width - 400;//sets the moon's X position
+            moon.y = groundY - 200; //sets moon's Y position 
+            moon.scaleX = 0.50; // scales the moon's width
+            moon.scaleY = 0.50; // scales the moon's height
+            background.addChild(moon);// add the mppm to the background container
             
             
             // TODO 4: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
-            
+            for (var i = 0; i < 5; ++i) { // for loop that creates a set number of buildings
+                var buildingColors = ["Pink", "LightBlue", "Red", "Black", "Orange"];
+                var buildingHeight = 500 * Math.random(); // a variable called buildingHeight that stores the y value of the building
+                var building = draw.rect(75, buildingHeight, buildingColors[i], "LightGray", "Black", 1); //Draws the building and has 5 arguments that shows the design of the building
+                building.x = 200 * i; // X value of the building 
+                building.y = groundY - buildingHeight; // y value of the building 
+                background.addChild(building); //adds the building to the background
+                buildings.push(building);// takes the building and pushes the building to the array
+            }
             
             // TODO 3: Part 1 - Add a tree
-            
+            tree = draw.bitmap("img/tree.png"); //creates a bitmap using the tree image and stores it in our variable tree
+            tree.x = 600// sets the x value of the tree
+            tree.y =groundY - 200; //sets the y value of the tree
+            background.addChild(tree); // adds the tree to the background container
             
         } // end of render function - DO NOT DELETE
         
@@ -63,10 +87,20 @@ var background = function (window) {
             var groundY = ground.y;
             
             // TODO 3: Part 2 - Move the tree!
-            
+            tree.x = tree.x - 1; // moves the tree to the left by subtracting from its current x position
+            // checks if the tree has gone off the left and resets to the right
+            if (tree.x < -200) {
+            tree.x = canvasWidth;
+            }
             
             // TODO 4: Part 2 - Parallax
-            
+            for(var i = 0; i < buildings.length; i++){ // every time this loop runs it pulls out an array
+                var building = buildings[i]; //array of budilings
+                building.x -= 1; // moves left
+                if (building.x < -200){ //checks if it moves off screen
+                    building.x = canvasWidth; //brings the buildings back
+                }
+            }
 
         } // end of update function - DO NOT DELETE
         
